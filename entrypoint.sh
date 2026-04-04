eee009d7a0430c489bf6549a4ede34a0e276d8ad
PNamGP1120 <phuongnam.it0212@gmail.com>
2026-04-04 10:36:36 +0700

    Allow database manager on Render

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,20 @@ set -e
 
 cat > /tmp/odoo-render.conf <<EOF
 [options]
-admin_passwd = ${ODOO_ADMIN_PASSWD:-admin123}
+admin_passwd = ${ODOO_ADMIN_PASSWD:-admin123456}
 db_host = ${DB_HOST}
 db_port = ${DB_PORT:-5432}
 db_user = ${DB_USER}
 db_password = ${DB_PASSWORD}
-db_name = False
+list_db = False
 addons_path = /mnt/extra-addons,/usr/lib/python3/dist-packages/odoo/addons
 data_dir = /var/lib/odoo
 proxy_mode = True
-list_db = True
 http_port = 8069
 EOF
 
-exec odoo -c /tmp/odoo-render.conf
+if [ "${INIT_ODOO_DB:-false}" = "true" ]; then
+  exec odoo -c /tmp/odoo-render.conf -d "${ODOO_DB_NAME}" -i base --without-demo=all
+else
+  exec odoo -c /tmp/odoo-render.conf -d "${ODOO_DB_NAME}"
+fi
